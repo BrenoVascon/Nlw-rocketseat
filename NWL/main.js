e91700c650d7e7bbc78aff972e15541cbf4fda25
brenoxdd4 <brenothegamer14@hotmail.com>
2022-06-30 21:23:37 -0300

    etapa final

--- NWL/main.js
+++ NWL/main.js
@@ -4,6 +4,43 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  //activateMenuAtCurrentSection(services)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // o topo da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alto
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // informações dos dados
+  console.log(
+    'O topo da seção chegou ou passou da linha',
+    sectionTopReachOrPassedTargetLine
+  )
+
+  // verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a seção termina aonde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  // limites da seção seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine &&
 }
 
 function showNavOnScroll() {
